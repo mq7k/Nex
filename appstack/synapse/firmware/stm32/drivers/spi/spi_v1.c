93a8b651211eabad924e3c69fd8976f35c660952
mq7k <247589825+mq7k@users.noreply.github.com>
2026-03-02 20:44:42 +0100

    Add map functions interface -> low-level driver

--- appstack/synapse/firmware/stm32/drivers/spi/spi_v1.c
+++ appstack/synapse/firmware/stm32/drivers/spi/spi_v1.c
@@ -1,4 +1,6 @@
 #include "synapse/stm32/drivers/spi/spi_v1.h"
+#include "libcom/format.h"
+#include "libcom/util.h"
 #include "synapse/common/util/common.h"
 #include "libcom/sys/devmode.h"
 #include "libcom/types.h"
@@ -953,6 +955,187 @@ spiif_get_capabilities(void)
   return cap;
 }
 
+static u32
+_map_phaseif(
+  enum spiif_clk_phase phaseif,
+  enum spi_clock_phase* phase
+)
+{
+  switch (phaseif)
+  {
+    case SPIIF_CLK_PHASE_FIRST:
+      *phase = SPI_CLOCK_PHASE_FIRST_TRANSITION;
+      break;
+      
+    case SPIIF_CLK_PHASE_SECOND:
+      *phase = SPI_CLOCK_PHASE_SECOND_TRANSITION;
+      break;
+
+    default:
+      return NEX_FAILURE;
+  }
+
+  return NEX_SUCCESS;
+}
+
+static u32
+_map_polarityif(
+  enum spiif_clk_polarity polarityif,
+  enum spi_clock_polarity* polarity
+)
+{
+  switch (polarityif)
+  {
+    case SPIIF_CLK_POLARITY_CLK_IDLE0:
+      *polarity = SPI_CLOCK_POLARITY_LOW_ON_IDLE;
+      break;
+
+    case SPIIF_CLK_POLARITY_CLK_IDLE1:
+      *polarity = SPI_CLOCK_POLARITY_HIGH_ON_IDLE;
+      break;
+
+    default:
+      return NEX_FAILURE;
+  }
+
+  return NEX_SUCCESS;
+}
+
+static u32
+_map_roleif(
+  enum spiif_role roleif,
+  enum spi_role* role
+)
+{
+  switch (roleif)
+  {
+    case SPIIF_ROLE_MASTER:
+      *role = SPI_ROLE_MASTER;
+      break;
+
+    case SPIIF_ROLE_SLAVE:
+      *role = SPI_ROLE_SLAVE;
+      break;
+
+    default:
+      return NEX_FAILURE;
+  }
+
+  return NEX_SUCCESS;
+}
+
+static u32
+_map_baudrateif(
+  enum spiif_baudrate baudrateif,
+  enum spi_baudrate_control* baudrate
+)
+{
+  switch (baudrateif)
+  {
+    case SPIIF_BAUDRATE_CLK_DIV2:
+      *baudrate = SPI_BAUDRATE_CONTROL_DIV2;
+      break;
+
+    case SPIIF_BAUDRATE_CLK_DIV4:
+      *baudrate = SPI_BAUDRATE_CONTROL_DIV4;
+      break;
+
+    case SPIIF_BAUDRATE_CLK_DIV8:
+      *baudrate = SPI_BAUDRATE_CONTROL_DIV8;
+      break;
+
+    case SPIIF_BAUDRATE_CLK_DIV16:
+      *baudrate = SPI_BAUDRATE_CONTROL_DIV16;
+      break;
+
+    case SPIIF_BAUDRATE_CLK_DIV64:
+      *baudrate = SPI_BAUDRATE_CONTROL_DIV64;
+      break;
+
+    case SPIIF_BAUDRATE_CLK_DIV128:
+      *baudrate = SPI_BAUDRATE_CONTROL_DIV128;
+      break;
+
+    case SPIIF_BAUDRATE_CLK_DIV256:
+      *baudrate = SPI_BAUDRATE_CONTROL_DIV256;
+      break;
+
+    default:
+      return NEX_FAILURE;
+  }
+
+  return NEX_SUCCESS;
+}
+
+static u32
+_map_formatif(
+  enum spiif_frame_fmt formatif,
+  enum spi_frame_format* format
+)
+{
+  switch (formatif)
+  {
+    case SPIIF_FRAME_FMT_MSB_FIRST:
+      *format = SPI_FRAME_FORMAT_MSB_FIRST;
+      break;
+
+    case SPIIF_FRAME_FMT_LSB_FIRST:
+      *format = SPI_FRAME_FORMAT_LSB_FIRST;
+      break;
+
+    default:
+      return NEX_FAILURE;
+  }
+
+  return NEX_SUCCESS;
+}
+
+static u32
+_map_format_lenif(
+  enum spiif_frame_len frame_lenif,
+  enum spi_data_frame_format* frame_len
+)
+{
+  switch (frame_lenif)
+  {
+    case SPIIF_FRAME_LEN_8BIT:
+      *frame_len = SPI_DATA_FRAME_FORMAT_8BITS;
+      break;
+
+    case SPIIF_FRAME_LEN_16BIT:
+      *frame_len = SPI_DATA_FRAME_FORMAT_16BITS;
+      break;
+
+    default:
+      return NEX_FAILURE;
+  }
+
+  return NEX_SUCCESS;
+}
+
+static u32
+_map_standardif(
+  enum spiif_frame_type typeif,
+  enum spi_frame_standard* standard
+)
+{
+  switch (typeif)
+  {
+    case SPIIF_FRAME_TYPE_MOTOROLA:
+      *standard = SPI_FRAME_STANDARD_MOTOROLA;
+      break;
+
+    case SPIIF_FRAME_TYPE_TI:
+      *standard = SPI_FRAME_STANDARD_TI;
+      break;
+
+    default:
+      return NEX_FAILURE;
+  }
+
+  return NEX_SUCCESS;
+}
+
 enum spiif_code
 spiif_configure(
   struct spiif_config* config
@@ -963,15 +1146,57 @@ spiif_configure(
     return SPIIF_CODE_INVALID_CFG;
   }
 
-  spi_set_clock_phase(config->spi, config->clk_phase);
-  spi_set_clock_polarity(config->spi, config->clk_polarity);
-  spi_set_role(config->spi, config->role);
-  spi_set_baudrate_control(config->spi, config->baudrate);
-  spi_set_frame_format(config->spi, config->frame_fmt);
-  spi_set_data_frame_format(config->spi, config->frame_len);
+  enum spi_clock_phase phase;
+  if (_map_phaseif(config->clk_phase, &phase) != NEX_SUCCESS)
+  {
+    return SPIIF_CODE_INVALID_CLK_PHASE;
+  }
+
+  enum spi_clock_polarity polarity;
+  if (_map_polarityif(config->clk_polarity, &polarity) != NEX_SUCCESS)
+  {
+    return SPIIF_CODE_INVALID_CLK_POLARITY;
+  }
+
+  enum spi_role role;
+  if (_map_roleif(config->role, &role) != NEX_SUCCESS)
+  {
+    return SPIIF_CODE_INVALID_ROLE;
+  }
+
+  enum spi_baudrate_control baudrate;
+  if (_map_baudrateif(config->baudrate, &baudrate) != NEX_SUCCESS)
+  {
+    return SPIIF_CODE_INVALID_BAUDRATE;
+  }
+
+  enum spi_frame_format format;
+  if (_map_formatif(config->frame_fmt, &format) != NEX_SUCCESS)
+  {
+    return SPIIF_CODE_INVALID_FRAME_FMT;
+  }
+
+  enum spi_data_frame_format frame_len;
+  if (_map_format_lenif(config->frame_len, &frame_len) != NEX_SUCCESS)
+  {
+    return SPIIF_CODE_INVALID_FRAME_LEN;
+  }
+
+  spi_set_clock_phase(config->spi, phase);
+  spi_set_clock_polarity(config->spi, polarity);
+  spi_set_role(config->spi, role);
+  spi_set_baudrate_control(config->spi, baudrate);
+  spi_set_frame_format(config->spi, format);
+  spi_set_data_frame_format(config->spi, frame_len);
 
 #if defined(STM32_SPI_FRAME_STANDARD)
-  spi_set_frame_standard(config->spi, config->frame_type);
+  enum spi_frame_standard standard;
+  if (_map_standardif(config->frame_type, &standard) != NEX_SUCCESS)
+  {
+    return SPIIF_CODE_INVALID_FRAME_TYPE;
+  }
+
+  spi_set_frame_standard(config->spi, standard);
 #endif
 
   if (config->options & SPIIF_CAP_HWCRC)
