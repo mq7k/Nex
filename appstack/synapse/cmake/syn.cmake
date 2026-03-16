4b9604861a786717e8eecd27d4f2aafb9e208ff5
mq7k <247589825+mq7k@users.noreply.github.com>
2026-03-17 00:50:29 +0100

    fix: link against libc and libgcc

--- appstack/synapse/cmake/syn.cmake
+++ appstack/synapse/cmake/syn.cmake
@@ -20,7 +20,12 @@ nex_define_macros_iraw(nex_buildcfg ${NEX_MCU_FORMATTED_GROUPS})
 target_link_libraries(nex_synapse PUBLIC nex_synapse_soc)
 target_link_libraries(nex_synapse PUBLIC nex_buildcfg)
 target_link_libraries(nex_synapse PUBLIC nex_libcom)
+
+# Software float/double operations.
 target_link_libraries(nex_synapse PUBLIC gcc)
+
+# String functions.
+target_link_libraries(nex_synapse PUBLIC c)
 
 # if (NEX_BUILD_EXAMPLES)
 #   include(appstack/nex_synapse/cmake/post/examples)
