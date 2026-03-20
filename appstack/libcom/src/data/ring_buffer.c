d413eb0799001a4e71b3a76fcc11951eb75d04fa
mq7k <247589825+mq7k@users.noreply.github.com>
2026-03-20 16:41:31 +0100

    fix: internal implementation inconsistencies
    - Head pointer not being wrapped around
    - find() returning absolute position instead of relative offset
    - Incorrect bytes_left check before wrapping
    - Wrong byte count being returned

--- appstack/libcom/src/data/ring_buffer.c
+++ appstack/libcom/src/data/ring_buffer.c
@@ -198,12 +198,13 @@ nex_ring_buffer_copy(
     count = sz;
   }
 
-  if (buffer->head + count < buffer->len)
+  if (buffer->head + count <= buffer->len)
   {
     // No need to wrap around.
     __builtin_memcpy(dst, &buffer->buffer[buffer->head], count);
-    buffer->size -= count;
-    buffer->head += count;
+    // buffer->size -= count;
+    // buffer->head += count;
+    _advance_head(buffer, count);
     return count;
   }
 
@@ -218,7 +219,7 @@ nex_ring_buffer_copy(
   buffer->size -= original_count;
   buffer->head = count;
 
-  return count;
+  return original_count;
 }
 
 u32
@@ -234,7 +235,7 @@ nex_ring_buffer_peek_copy(
     count = sz;
   }
 
-  if (buffer->head + count < buffer->len)
+  if (buffer->head + count <= buffer->len)
   {
     // No need to wrap around.
     __builtin_memcpy(dst, &buffer->buffer[buffer->head], count);
@@ -251,7 +252,7 @@ nex_ring_buffer_peek_copy(
   count -= bytes_left_before_wrap;
   __builtin_memcpy(dst + bytes_left_before_wrap, buffer->buffer, count);
 
-  _advance_head(buffer, original_count);
+  // _advance_head(buffer, original_count);
   return original_count;
 }
 
@@ -262,7 +263,7 @@ nex_ring_buffer_write_bytes(
   u32 count
 )
 {
-  u32 size = nex_ring_buffer_len(buffer);
+  u32 size = nex_ring_buffer_space_left(buffer);
   if (count > size)
   {
     count = size;
@@ -274,8 +275,9 @@ nex_ring_buffer_write_bytes(
   {
     // No need to wrap around.
     __builtin_memcpy(&buffer->buffer[buffer->tail], src, count);
-    buffer->tail += count;
-    buffer->size += count;
+    // buffer->tail += count;
+    _advance_tail(buffer, count);
+    // buffer->size += count;
     // printf("[PC] All written, returning %u\n", count);
     return count;
   }
@@ -306,7 +308,7 @@ nex_ring_buffer_find(
     u32 idx = FAST_MOD(buffer->head + i, buffer->len);
     if (buffer->buffer[idx] == byte)
     {
-      *pos = i;
+      *pos = (idx - starting_at);
       return NEX_SUCCESS;
     }
   }
