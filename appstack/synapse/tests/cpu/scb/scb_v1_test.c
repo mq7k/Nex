e0a9b41a0c07cf7358f62ea7a612df8245c1110c
mq7k <247589825+mq7k@users.noreply.github.com>
2026-03-21 22:23:14 +0100

    fix: new generated tests

--- appstack/synapse/tests/cpu/scb/scb_v1_test.c
+++ appstack/synapse/tests/cpu/scb/scb_v1_test.c
@@ -4,6 +4,7 @@
 #include "synapse/cpu/cortex/drivers/scb/scb_v1.h"
 #include "libtest/libtest.h"
 
+
 void
 setup(void)
 {
@@ -35,12 +36,39 @@ test_scb_interrupt_multi_cycle_intructions(void)
 
 }
 
+// └─Skipping type 'fn_toggle (TODO)' (scb_write_buffer)
+void
+test_scb_folding_it_instructions(void)
+{
+  SCB->ACTLR = 0;
+  scb_folding_it_instructions_disable();
+  ASSERT_EQ(SCB->ACTLR, (1u << 2));
+  ASSERT_FALSE(execution_halted());
+
+  SCB->ACTLR = ~(1u << 2);
+  scb_folding_it_instructions_disable();
+  ASSERT_EQ(SCB->ACTLR, 0xffffffff);
+  ASSERT_FALSE(execution_halted());
+
+  SCB->ACTLR = (1u << 2);
+  scb_folding_it_instructions_enable();
+  ASSERT_EQ(SCB->ACTLR, 0);
+  ASSERT_FALSE(execution_halted());
+
+  SCB->ACTLR = 0xffffffff;
+  scb_folding_it_instructions_enable();
+  ASSERT_EQ(SCB->ACTLR, ~(1u << 2));
+  ASSERT_FALSE(execution_halted());
+
+}
+
 int
 main(void)
 {
   const test_function_t tests[] =
   {
     TEST_FUNC(test_scb_interrupt_multi_cycle_intructions),
+    TEST_FUNC(test_scb_folding_it_instructions),
   };
 
   return TESTS_RUN(tests);
