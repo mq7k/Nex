95933d567b0a954ab4913b467e7c46c65c49ea10
mq7k <247589825+mq7k@users.noreply.github.com>
2026-03-28 02:38:21 +0100

    fix: dshot example

--- examples/stm32f4/dshot/main.c
+++ examples/stm32f4/dshot/main.c
@@ -1,27 +1,22 @@
-/*
- * This examples is currently disabled 
- * due to incorrect behavior.
- *
- * The code is kept for reference and will be fixed in a future revision.
- */
 #include "synapse/common/common.h"
 #include "libcom/types.h"
 
 #include "synapse/soc/stm32/periph/flash.h"
 #include "synapse/soc/stm32/periph/rcc.h"
 #include "synapse/soc/stm32/periph/gpio.h"
-#include "synapse/soc/stm32/periph/usart.h"
 #include "synapse/soc/stm32/periph/tim.h"
 #include "synapse/soc/stm32/periph/dma.h"
 #include "synapse/soc/stm32/periph/adc.h"
 #include "synapse/cpu/cortex/common/sys.h"
 #include "synapse/cpu/cortex/periph/systick.h"
+#include "synapse/cpu/cortex/periph/fpu.h"
+#include "synapse/cpu/cortex/periph/nvic.h"
 
 // DShot150
-#define TIM_AUTORELOAD_VALUE (667)
+// #define TIM_AUTORELOAD_VALUE (667)
 
 // DShot300
-// #define TIM_AUTORELOAD_VALUE (334)
+#define TIM_AUTORELOAD_VALUE (334)
 
 // DShot600
 // #define TIM_AUTORELOAD_VALUE (167)
@@ -32,7 +27,7 @@
 #define CC_PERIOD_HIGH ((u32) (TIM_AUTORELOAD_VALUE * .75))
 #define CC_PERIOD_LOW ((u32) (TIM_AUTORELOAD_VALUE * .375))
 
-#define PERIODS_ARRAY_LENGTH (18)
+#define PERIODS_ARRAY_LENGTH (17)
 u16 arr[PERIODS_ARRAY_LENGTH];
 
 struct dshot_config
@@ -45,13 +40,17 @@ struct dshot_config
 };
 
 u32
-dshot_motor_speed_percentage(u32 percentage)
+dshot_motor_speed_percentage(
+  u32 percentage
+)
 {
   return (u32) (((float) percentage / 100.0f) * 2000) + 48;
 }
 
 u32
-dshot_pack_frame(struct dshot_config* config)
+dshot_pack_frame(
+  struct dshot_config* config
+)
 {
   u32 frame = 0;
   frame |= ((config->motor_speed & 2047) << 1);
@@ -64,7 +63,10 @@ dshot_pack_frame(struct dshot_config* config)
 }
 
 void
-dshot_calc_periods(u32 frame, u16* buf)
+dshot_calc_periods(
+  u32 frame,
+  u16* buf
+)
 {
   for (u32 i = 0; i < 16; ++i)
   {
@@ -102,27 +104,11 @@ rcc_setup(void)
   rcc_system_clock_source_ready_wait(RCC_SYSTEM_CLOCK_SOURCE_PLL);
 
   rcc_periph_clock_enable(RCC_PERIPH_TIM1);
+  rcc_periph_clock_enable(RCC_PERIPH_TIM2);
   rcc_periph_clock_enable(RCC_PERIPH_GPIOA);
   rcc_periph_clock_enable(RCC_PERIPH_GPIOC);
   rcc_periph_clock_enable(RCC_PERIPH_DMA2);
-  rcc_periph_clock_enable(RCC_PERIPH_USART1);
   rcc_periph_clock_enable(RCC_PERIPH_ADC1);
-}
-
-void
-usart_setup(void)
-{
-  constexpr u32 baudrate = 115200;
-  const u32 clock = 100000000;
-  usart_set_baudrate(USART1, clock, baudrate);
-  usart_set_word_length(USART1, USART_WORD_LENGTH_8BITS);
-  usart_set_stop_bits(USART1, USART_STOP_BITS_1BIT);
-  usart_transmitter_enable(USART1);
-  usart_receiver_enable(USART1);
-  usart_parity_control_disable(USART1);
-  usart_cts_disable(USART1);
-  usart_rts_disable(USART1);
-  usart_enable(USART1);
 }
 
 void
@@ -130,34 +116,7 @@ gpio_setup(void)
 {
   gpio_setup_port_pins(
     GPIOA,
-    GPIO11,
-    GPIO_MODE_ALTFN,
-    GPIO_SPEED_HIGH
-  );
-
-  gpio_setup_port_pins(
-    GPIOC,
-    GPIO13,
-    GPIO_MODE_OUTPUT,
-    GPIO_SPEED_HIGH
-  );
-
-  gpio_set_port_pins_output_type(
-    GPIOC,
-    GPIO13,
-    GPIO_OUTPUT_TYPE_PUSHPULL
-  );
-
-  gpio_setup_port_pins(
-    GPIOA,
-    GPIO9,
-    GPIO_MODE_ALTFN,
-    GPIO_SPEED_HIGH
-  );
-
-  gpio_setup_port_pins(
-    GPIOA,
-    GPIO10,
+    GPIO9 | GPIO10 | GPIO11,
     GPIO_MODE_ALTFN,
     GPIO_SPEED_HIGH
   );
@@ -189,20 +148,57 @@ dma_setup(void)
 {
   dma_stream_disable(DMA2, DMA_STREAM5);
 
-  dma_stream_set_periph_address(DMA2, DMA_STREAM5, (u32) &TIM1->CCR4);
-  dma_stream_set_memory_address(DMA2, DMA_STREAM5, DMA_STREAM_MEMORY0, (u32) arr);
+  dma_stream_set_periph_address(
+    DMA2,
+    DMA_STREAM5,
+    (u32) &TIM1->CCR4
+  );
+
+  dma_stream_set_memory_address(
+    DMA2,
+    DMA_STREAM5,
+    DMA_STREAM_MEMORY0,
+    (u32) arr
+  );
+
   dma_stream_set_items_transfer_count(DMA2, DMA_STREAM5, ARR_SIZE(arr));
   dma_stream_set_channel(DMA2, DMA_STREAM5, DMA_STREAM_CHANNEL6);
   dma_stream_set_priority(DMA2, DMA_STREAM5, DMA_STREAM_PRIORITY_VERY_HIGH);
   dma_stream_direct_mode_enable(DMA2, DMA_STREAM5);
 
-  dma_stream_set_data_transfer_direction(DMA2, DMA_STREAM5, DMA_STREAM_DIRECTION_MEMORY_TO_PERIPH);
+  dma_stream_set_data_transfer_direction(
+    DMA2,
+    DMA_STREAM5,
+    DMA_STREAM_DIRECTION_MEMORY_TO_PERIPH
+  );
+
   dma_stream_memory_increment_mode_enable(DMA2, DMA_STREAM5);
   dma_stream_periph_increment_mode_disable(DMA2, DMA_STREAM5);
-  dma_stream_set_memory_burst_transfer(DMA2, DMA_STREAM5, DMA_STREAM_MEMORY_BURST_SINGLE_TRANSFER);
-  dma_stream_set_periph_burst_transfer(DMA2, DMA_STREAM5, DMA_STREAM_PERIPH_BURST_SINGLE_TRANSFER);
-  dma_stream_set_memory_data_size(DMA2, DMA_STREAM5, DMA_STREAM_MEMORY_SIZE_HALF_WORD);
-  dma_stream_set_periph_data_size(DMA2, DMA_STREAM5, DMA_STREAM_PERIPH_SIZE_HALF_WORD);
+
+  dma_stream_set_memory_burst_transfer(
+    DMA2,
+    DMA_STREAM5,
+    DMA_STREAM_MEMORY_BURST_SINGLE_TRANSFER
+  );
+
+  dma_stream_set_periph_burst_transfer(
+    DMA2,
+    DMA_STREAM5,
+    DMA_STREAM_PERIPH_BURST_SINGLE_TRANSFER
+  );
+
+  dma_stream_set_memory_data_size(
+    DMA2,
+    DMA_STREAM5,
+    DMA_STREAM_MEMORY_SIZE_HALF_WORD
+  );
+
+  dma_stream_set_periph_data_size(
+    DMA2,
+    DMA_STREAM5,
+    DMA_STREAM_PERIPH_SIZE_HALF_WORD
+  );
+
   dma_stream_circular_mode_disable(DMA2, DMA_STREAM5);
   dma_stream_enable(DMA2, DMA_STREAM5);
 }
@@ -227,6 +223,18 @@ timer_setup(void)
 }
 
 void
+timer_sys_setup(void)
+{
+  tim_set_prescaler(TIM2, 3);
+  tim_set_clock_division(TIM2, TIM_CLOCK_DIVISION_NODIV);
+  tim_set_autoreload_value(TIM2, 24);
+  tim_set_counter_direction(TIM2, TIM_COUNTER_DIRECTION_UP);
+  tim_set_cc_value(TIM2, TIM_CHANNEL4, 0);
+  tim_interrupt_enable(TIM2, TIM_INTERRUPT_UPDATE);
+  nvic_irq_enable(NVIC_IRQ_TIM2);
+}
+
+void
 adc_setup(void)
 {
   u32 sequence[] = { ADC_REG_CHANNEL0 };
@@ -235,51 +243,36 @@ adc_setup(void)
   adc_enable(ADC1);
 }
 
-void
-systick_setup(void)
-{
-  systick_set_clock_source(SYSTICK_CLOCK_SOURCE_PROCESSOR);
-  systick_set_reload_value(99);
-  systick_set_current_value(0);
-  systick_interrupt_enable();
-  systick_enable();
-}
-
-u32 delay;
-u32 tick;
+volatile u32 delay;
 
 void
-systick_handler(void)
+tim2_isr(void)
 {
+  tim_flag_clear(TIM2, TIM_FLAG_UPDATE);
+  nvic_clear_pending_irq(NVIC_IRQ_TIM2);
+
   if (delay > 0)
   {
     --delay;
   }
-
-  ++tick;
-
-  cm_isb();
-  cm_dsb();
 }
 
 void
 delay_us(u32 us)
 {
+  tim_set_counter_value(TIM2, 0);
   delay = us;
-  systick_interrupt_enable();
+  tim_counter_enable(TIM2);
   while (delay > 0);
-  systick_interrupt_disable();
+  tim_counter_disable(TIM2);
+
+  cm_dmb();
 }
 
 int
 main(void)
 {
-  syn_delay_nop(1000000);
-
-  for (u32 i = 0; i < PERIODS_ARRAY_LENGTH; ++i)
-  {
-    arr[i] = CC_PERIOD_LOW;
-  }
+  fpu_set_access_level(FPU_ACCESS_LEVEL_FULL);
 
   struct dshot_config conf;
   conf.motor_speed = dshot_motor_speed_percentage(0);
@@ -289,11 +282,10 @@ main(void)
 
   rcc_setup();
   gpio_setup();
-  usart_setup();
   adc_setup();
   dma_setup();
   timer_setup();
-  systick_setup();
+  timer_sys_setup();
 
   u32 speed = 0;
   u32 adc_data = 0;
@@ -309,12 +301,20 @@ main(void)
       {
         update_speed = 0;
         speed = (u32) ((float) adc_data / 4095.0f * 100.0f);
+
+        // If the ESC receives a throttle value > 2048,
+        // it immediately shuts down the motor.
+        if (speed > 100)
+        {
+          speed = 100;
+        }
+
         conf.motor_speed = dshot_motor_speed_percentage(speed);
         frame = dshot_pack_frame(&conf);
         dshot_calc_periods(frame, arr);
       }
 
-      delay_us(10);
+      delay_us(25);
       dma_stream_enable(DMA2, DMA_STREAM5);
     }
 
@@ -332,3 +332,4 @@ main(void)
     }
   }
 }
+
