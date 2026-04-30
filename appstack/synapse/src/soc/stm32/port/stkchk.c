6751b372517c0701d82a8b349cb4696cb0e9af4c
mq7k <247589825+mq7k@users.noreply.github.com>
2026-04-30 22:44:05 +0200

    fix: random stack canary generation

--- appstack/synapse/src/soc/stm32/port/stkchk.c
+++ appstack/synapse/src/soc/stm32/port/stkchk.c
@@ -1,4 +1,5 @@
 #include "synapse/common/stkchk.h"
+#include "soc/stm32/drivers/rcc/rcc_v2.h"
 #include "synapse/soc/stm32/system/specs.h"
 
 #if defined(STM32_RNG)
@@ -16,8 +17,21 @@ syn_stkchk_guard_generate(void)
   return syn_stkchk_guard_generate_def();
 #else
 
-  // TODO: Not tested due to lack of physical hardware.
   rcc_periph_clock_enable(RCC_PERIPH_RNG);
+
+  // The RNG requires a clock source <= 48MHz.
+  // It uses the Q output of the main PLL.
+  // 16 HSI / 8 = VCO input frequency (The result must be between 1 and 2 MHz)
+  // VCO output frequency = VCO input frequency * 50 (The result must be between 100 and 432 MHz)
+  // RNG clock = VCO output frequency / 4 = 20 MHz
+  rcc_set_main_pll_factorM(8);
+  rcc_set_main_pll_factorN(50);
+  rcc_set_main_pll_factorQ(4);
+  rcc_set_main_pll_source(RCC_MAIN_PLL_SOURCE_HSI);
+
+  rcc_osc_enable(RCC_OSC_PLL);
+  while (!rcc_is_osc_ready(RCC_OSC_PLL));
+
   rng_enable();
 
   u32 random;
@@ -28,6 +42,8 @@ syn_stkchk_guard_generate(void)
 
   rcc_periph_reset(RCC_PERIPH_RNG);
   rcc_periph_clock_disable(RCC_PERIPH_RNG);
+
+  rcc_osc_disable(RCC_OSC_PLL);
 
   return random;
 #endif
